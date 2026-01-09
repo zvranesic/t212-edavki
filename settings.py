47a75c399c65443cc39a9584a260eba4d6b6cc48
zvranesic <zan.vranesic1@gmail.com>
2026-01-09 22:47:28 +0100

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,22 @@
+# --- NASTAVITVE IZRAČUNA ---
+TAX_YEAR = 2025
+TAX_RATE = 0.25  # 25% davek na dobiček
+
+# --- OSEBNI PODATKI ZA EDAVKI ---
+TAX_NUMBER = "45645645"
+NAME = "Janez Novak"
+ADDRESS = "Davčna ul. 1"
+CITY = "Ljubljana"
+POST_NUMBER = "1000"
+POST_NAME = "Ljubljana"
+EMAIL = "janez.novak@gmail.com"
+PHONE = "031234567"
+
+# Mape
+INPUT_FOLDER = './input'
+OUTPUT_FOLDER = './output'
+
+# Logika za split (npr. NVDA 1:10)
+STOCK_SPLITS = [
+    ('US67066G1040', '2024-06-10', 10.0),
+]
